867f40913fd493c60cd74c168adc892d299a20e2
pspremnath <premnath.xybion@gmail.com>
2022-12-02 08:16:27 +0530

    third commit

--- src/main/java/com/igmi/main/Test.java
+++ src/main/java/com/igmi/main/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String args[]) {
 		
 		System.out.println("This Project is a Docker Jenkins Integration");
+		System.out.println("This Project is a Docker Jenkins Integration2");
 		
 	}
 
